57b27f33d1f66bc2352eab8aadbd6ec05728b5a1
duradurilkina-afk <duradurilkina@gmail.com>
2026-06-14 21:38:36 +0700

    update

--- server.js
+++ server.js
@@ -2,23 +2,30 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors: { origin: "*" } // Разрешаем подключения со всех доменов
+    cors: { origin: "*" }
 });
 
 const PORT = process.env.PORT || 80;
 
+// Храним список подключенных игроков в памяти сервера
+let players = {};
+
 io.on('connection', (socket) => {
     console.log('Игрок подключился. ID:', socket.id);
+    players[socket.id] = { position: { x: 0, y: 0, z: 0 } };
 
-    // 1. Сообщаем ВСЕМ, что зашел новый игрок
+    // 1. Отправляем новичку список всех, кто УЖЕ играет
+    socket.emit('currentPlayers', players);
+
+    // 2. Оповещаем остальных о приходе новичка
     socket.broadcast.emit('playerJoined', {
         id: socket.id,
         position: { x: 0, y: 0, z: 0 }
     });
 
-    // 2. Обработка отключения
     socket.on('disconnect', () => {
         console.log('Игрок отключился:', socket.id);
+        delete players[socket.id];
         socket.broadcast.emit('playerLeft', { id: socket.id });
     });
 });
