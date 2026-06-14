d9c2cc913a8d4be1cd9839bab3be3e29e47d8821
duradurilkina-afk <duradurilkina@gmail.com>
2026-06-14 23:58:46 +0700

    update

--- server.js
+++ server.js
@@ -3,41 +3,47 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "*", // Разрешаем доступ со всех доменов
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
 
 const PORT = process.env.PORT || 80;
 
-// Хранилище всех подключенных игроков
+// Хранилище всех игроков
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Игрок подключился. ID:', socket.id);
+    console.log('Новое подключение. ID:', socket.id);
 
-    // Добавляем игрока в список с начальной позицией
+    // Защита: если ID вдруг совпал (редко при реконнекте), очищаем старые данные
+    if (players[socket.id]) {
+        console.log('Внимание: дубликат ID, перезаписываем:', socket.id);
+    }
+
+    // Инициализируем данные нового игрока
     players[socket.id] = { position: { x: 0, y: 0, z: 0 } };
 
-    // 1. Отправляем только что подключившемуся игроку список всех существующих игроков
+    // 1. Отправляем только что зашедшему игроку список всех, кто уже в игре
     socket.emit('currentPlayers', players);
 
-    // 2. Оповещаем всех остальных участников, что появился новый игрок
+    // 2. Оповещаем всех остальных (кроме самого новичка) о том, что зашел новый игрок
     socket.broadcast.emit('newPlayer', {
         id: socket.id,
         position: { x: 0, y: 0, z: 0 }
     });
 
-    // Обработка отключения игрока
+    // Обработка отключения
     socket.on('disconnect', () => {
         console.log('Игрок отключился:', socket.id);
-        delete players[socket.id];
-        // Сообщаем всем остальным, что игрок ушел
-        socket.broadcast.emit('playerLeft', { id: socket.id });
+        if (players[socket.id]) {
+            delete players[socket.id];
+            // Оповещаем остальных, что игрок вышел
+            socket.broadcast.emit('playerLeft', { id: socket.id });
+        }
     });
 });
 
-// Запуск сервера
 http.listen(PORT, () => {
     console.log('Сервер успешно запущен на порту ' + PORT);
 });
