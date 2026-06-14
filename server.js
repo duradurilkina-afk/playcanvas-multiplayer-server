6b75e3c0a7af63b079a0b8fa1094a397fb85ff97
duradurilkina-afk <duradurilkina@gmail.com>
2026-06-15 01:07:29 +0700

    update

--- server.js
+++ server.js
@@ -12,21 +12,32 @@ let players = {};
 io.on('connection', (socket) => {
     console.log('Подключение:', socket.id);
 
-    // Добавляем в список
     players[socket.id] = { position: { x: 0, y: 0, z: 0 } };
 
-    // 1. Новичок получает список ТОЛЬКО СЕЙЧАС (один раз)
+    // Отправляем новичку список всех уже играющих
     socket.emit('currentPlayers', players);
 
-    // 2. Всем остальным сообщаем о новом игроке
+    // Оповещаем остальных о новом игроке
     socket.broadcast.emit('newPlayer', { id: socket.id, position: { x: 0, y: 0, z: 0 } });
 
+    // --- ВОТ СЮДА ВСТАВЛЯЕМ ОБРАБОТЧИК ДВИЖЕНИЯ ---
+    socket.on('playerMoved', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].position = data.position;
+            data.id = socket.id;
+            // Рассылаем всем остальным новые координаты
+            socket.broadcast.emit('playerMoved', data);
+        }
+    });
+    // ----------------------------------------------
+
     socket.on('disconnect', () => {
+        console.log('Отключился:', socket.id);
         delete players[socket.id];
         socket.broadcast.emit('playerLeft', { id: socket.id });
     });
 });
 
 http.listen(PORT, () => {
-    console.log('Сервер запущен');
+    console.log('Сервер запущен на порту ' + PORT);
 });
