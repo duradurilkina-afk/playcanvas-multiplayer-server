c4cebd8b591f10e4ff36b91bfe0012eda37fbb27
duradurilkina-afk <duradurilkina@gmail.com>
2026-06-15 14:32:22 +0700

    update

--- server.js
+++ server.js
@@ -2,33 +2,33 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    },
-    transports: ['websocket', 'polling'] // Сервер примет и то, и то, но приоритет за сокетами
+    cors: { origin: "*", methods: ["GET", "POST"] },
+    transports: ['websocket']
 });
 
 const PORT = process.env.PORT || 80;
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('=== СЕРВЕР: Подключился игрок с ID:', socket.id);
+    console.log('Подключился: ', socket.id);
 
-    players[socket.id] = {
-        position: { x: 0, y: 0, z: 0 },
-        rotation: { x: 0, y: 0, z: 0 },
-        speed: 0
-    };
+    // Создаем пустую запись, ждем координат инициализации
+    players[socket.id] = null;
 
-    // Отправляем вошедшему список игроков
-    socket.emit('currentPlayers', players);
-
-    // Оповещаем остальных
-    socket.broadcast.emit('newPlayer', {
-        id: socket.id,
-        position: { x: 0, y: 0, z: 0 },
-        rotation: { x: 0, y: 0, z: 0 }
+    socket.on('initPosition', (data) => {
+        players[socket.id] = {
+            position: data.position,
+            rotation: data.rotation,
+            speed: 0
+        };
+        // Отдаем вошедшему список текущих игроков
+        socket.emit('currentPlayers', players);
+        // Оповещаем остальных с правильными координатами
+        socket.broadcast.emit('newPlayer', {
+            id: socket.id,
+            position: data.position,
+            rotation: data.rotation
+        });
     });
 
     socket.on('playerMoved', (data) => {
@@ -36,19 +36,16 @@ io.on('connection', (socket) => {
             players[socket.id].position = data.position;
             players[socket.id].rotation = data.rotation;
             data.id = socket.id;
-
-            // Транслируем движение остальным
             socket.broadcast.emit('playerMoved', data);
         }
     });
 
     socket.on('disconnect', () => {
-        console.log('=== СЕРВЕР: Отключился игрок с ID:', socket.id);
         delete players[socket.id];
         socket.broadcast.emit('playerLeft', { id: socket.id });
     });
 });
 
 http.listen(PORT, () => {
-    console.log('=== СЕРВЕР ЗАПУЩЕН НА ПОРТУ ' + PORT + ' ===');
+    console.log('Сервер работает на порту ' + PORT);
 });
